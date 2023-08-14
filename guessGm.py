a90fb9c45163982cdbef91d67e729b7a8f5f3e8b
mmm404 <112262711+mmm404@users.noreply.github.com>
2023-08-14 19:22:47 +0300

    Add files via upload

---
+++ guessGm.py
@@ -0,0 +1,29 @@
+from random import randint
+
+true_guess=randint(1, 10)
+
+user_guess=input('Enter a numerical guess between 1-10')
+
+
+
+
+while True:
+  
+  try:
+   
+    if true_guess==int(user_guess):
+     
+      print('Hurray! you are the genius ')
+
+ 
+    else: 
+     
+      print('You lose, Do you wish to proceed with the game? (Y/N)')
+ 
+  except ValueError as err:
+    
+    print('your guess should be numerical')
+    
+
+
+
