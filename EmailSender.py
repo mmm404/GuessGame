b2930512e97dfcf0bb65f4bedc058b92b923beb7
mmm404 <112262711+mmm404@users.noreply.github.com>
2023-08-14 19:31:43 +0300

    Add files via upload

---
+++ EmailSender.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+
+from smtplib import SMTP
+
+email = EmailMessage()
+
+email['Subject'] = 'Confirmation message'
+
+email['from'] = 'mmms404'
+
+email['to'] = '9sammmmmmm@gmail.com'
+
+email.set_content('wow, you are the winner!')
+
+with SMTP(host='smtp.gmail.com', port=587) as mail:
+ 
+  mail.ehlo()
+ 
+  mail.starttls()
+  
+  mail.login('9sammmmmm@gmail.com', '_Mmms404#')
+
+  mail.sendmail()
